79ff5111d65a7d2235413d014776e1f653020e5f
Felipe Soto C <pipesoto13@gmail.com>
2020-10-21 01:36:01 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { count: 0};
+
+    this.countLetters = this.countLetters.bind(this);
+  }
+
+
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onKeyUp={this.countLetters}></textarea>
+    <div className="counter">{this.state.count}</div>
       </div>
     );
   }
+
+  countLetters = (event) => {
+    let text = event.target.value;
+    this.setState({count: text.length});
+  }
+
 }
 
 export default App;
